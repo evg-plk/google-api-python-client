e0df2f8add00ae9ea47908a78097e27e48301ff1
Evgeniy Polyak <evgeniy.polyak@gmail.com>
2018-02-08 12:48:46 +0300

    читаю свой файл

--- quikstart.py
+++ quikstart.py
@@ -37,15 +37,15 @@ def get_credentials():
     store = Storage(credential_path)
     credentials = store.get()
 
-    if not credentials or credentials.invelid:
+    if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
 
-    if flags:
-        credentials = tools.run_flow(flow, store, flags)
-    else:  # Needed only for compatibility with Python 2.6
-        credentials = tools.run(flow, store)
-    print('Storing credentials to ' + credential_path)
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
 
     return credentials
 
@@ -62,10 +62,9 @@ def main():
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
     service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
 
-    spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-    rangeName = 'Class Data!A2:E'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    spreadsheetId = '1_o9ojwy_cr7N8pv6QsGMJod8TvzRfqN_75szbNaxgCM'
+    rangeName = 'Груза!B2:G'
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
     values = result.get('values', [])
 
     if not values:
@@ -73,8 +72,8 @@ def main():
     else:
         print('Name, Major:')
         for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+            s = '\t'.join(row)
+            print(s)
 
 
 if __name__ == '__main__':
